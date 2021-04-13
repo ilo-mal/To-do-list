4ec46fbfbdf3c3854b8072784016701dc84bbc89
ilo-mal <67440783+ilo-mal@users.noreply.github.com>
2021-04-13 14:32:02 +0200

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -1,5 +1,3 @@
-# Write your code here
-# Write your code here
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
